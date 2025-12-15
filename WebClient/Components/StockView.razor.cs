c9dd810471089d9aacf9b8eba127e96c98324357
laptop_JSebas11 <juans3.jsda@gmail.com>
2025-12-14 21:20:51 -0500

    Simple GUI  FInished

--- WebClient/Components/StockView.razor.cs
+++ WebClient/Components/StockView.razor.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Components;
+using MudBlazor;
 using Shares.DTOs;
+using Shares.Enums;
 
 namespace WebClient.Components;
 
@@ -8,7 +10,58 @@ public partial class StockView {
     [Parameter]
     public TickerDto? Ticker { get; set; }
 
+    //--------------------------aux.Fields--------------------------
+    private Color _marketStateColor = Color.Surface;
+    private string? _quoteTypeImgSrc;
+    private string _updatedIcon = Icons.Material.Outlined.BrowserUpdated;
+    private readonly Dictionary<string, object?> _financialDict = [];
+    private readonly Dictionary<string, object?> _fundamentalsDict = [];
 
     //--------------------------METHODS--------------------------
-    
+    protected override void OnInitialized() {
+        if (Ticker is null) return;
+        
+        DefineGraphicItems();
+        DefineMetricDicts();
+    }
+
+    private void DefineGraphicItems() {
+        // Definicion de elementos graficos que seran dinamicos de acuerdo a ciertas condiciones
+        _updatedIcon = Ticker?.LastUpdated.ToLocalTime().Date == DateTime.Today.Date 
+            ? Icons.Material.Outlined.EventAvailable 
+            : Icons.Material.Outlined.BrowserUpdated;
+
+        _marketStateColor = Ticker?.MarketState switch {
+            MarketState.Regular => Color.Success,
+            MarketState.Closed => Color.Error,
+            MarketState.Post or MarketState.Pre or MarketState.Unknown => Color.Warning,
+            _ => Color.Warning
+        };
+
+        _quoteTypeImgSrc = Ticker?.QuoteType switch {
+            QuoteType.Equity => "imgs/stock_icon.png",
+            QuoteType.ETF => "imgs/etf_icon.png",
+            QuoteType.CryptoCurrency => "imgs/crypto_icon.webp",
+            QuoteType.Fund => "imgs/fund_icon.png",
+            QuoteType.Index => "imgs/index_icon.png",
+            QuoteType.Unknown => "imgs/generic_icon.png",
+            _ => "imgs/generic_icon.png"
+        };
+    }
+
+    private void DefineMetricDicts() {
+        // FINANCIAL DICTIONARY
+        _financialDict["Volume"] = Ticker?.RegularMarketVolume;
+        _financialDict["Capitalization"] = Ticker?.MarketCap;
+        _financialDict["52 weeks highest"] = Ticker?.FiftyTwoWeekHigh;
+        _financialDict["52 weeks lowest"] = Ticker?.FiftyTwoWeekLow;
+        
+        // FINANCIAL DICTIONARY
+        _fundamentalsDict["EPS TTM"] = Ticker?.EpsTtm;
+        _fundamentalsDict["EPS Forward"] = Ticker?.EpsForward;
+        _fundamentalsDict["PE Forward"] = Ticker?.ForwardPE;
+        _fundamentalsDict["Books to Price"] = Ticker?.Price2Book;
+        _fundamentalsDict["Book Value"] = Ticker?.BookValue;
+        _fundamentalsDict["Shares Outstanding"] = Ticker?.SharesOutstanding;
+    }
 }
--- WebClient/Pages/Home.razor.cs
+++ WebClient/Pages/Home.razor.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Components;
 using MudBlazor;
 using Shares.DTOs;
+using Shares.Results;
 using WebClient.Services.API.ApiService;
 
 namespace WebClient.Pages;
@@ -15,6 +16,7 @@ public partial class Home {
     private TickerDto? _ticker;
     private string? _symbol = string.Empty;
     private string _infoMsg = "Nothing to show...";
+    private Color _infoColor = Color.Warning;
     private bool _stockLoaded = false;
     private bool _isLoading = false;
 
@@ -22,23 +24,54 @@ public partial class Home {
     private async Task GetAssetAsync() {
         _isLoading = true;
         _stockLoaded = false;
+        _infoColor = Color.Warning;
 
         try {
             if (!IsTickerValid()) return;
 
             _ticker = await ApiService.GetTickerAsync(_symbol);
             _stockLoaded = true;
+            _infoMsg = "Nothing to show...";
         } 
+        catch(ApiException ex) { 
+            _infoMsg = ex.StatusCode switch {
+                404 => $"Asset with symbol ({_symbol}) not found",
+                500 => $"Internal error in API ({ex.Message})",
+                _ => ex.Message
+            };
+        }
         catch(Exception ex) { _infoMsg = ex.Message; }
         finally { _isLoading = false; }
     }
     private async Task UpdateAssetAsync() {
         _isLoading = true;
+        _stockLoaded = false;
+        _infoColor = Color.Warning;
 
         try {
             if (!IsTickerValid()) return;
-            await Task.Delay(1000);
+
+            Result result = await ApiService.RefreshTickerAsync(_symbol);
+
+            if (result.Success) {
+                _infoMsg = result.Description;
+                _infoColor = Color.Success;
+            }
         }
+        catch(ApiException ex) { 
+            _infoMsg = ex.StatusCode switch {
+                404 => $"Asset with symbol ({_symbol}) does not exist in Database",
+                500 => $"Internal error in API ({ex.Message})",
+                _ => ex.Message
+            };
+
+            // Lanza excepcion incluso en exito por serializacion de Result (solucion temporal)
+            if (ex.StatusCode == 200) {
+                _infoMsg = $"Asset ({_symbol}) has been updated successfully";
+                _infoColor = Color.Success;
+            }
+        }
+        catch(Exception ex) { _infoMsg = ex.Message; }
         finally { _isLoading = false; }
     }
 
